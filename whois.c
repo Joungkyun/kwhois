28e189f3bcaf060c0a1a6f25229f5d67ec21bf90
oops <oops@6fe69342-65cd-11e0-a88a-bcf54456e0af>
2015-01-17 10:04:51 +0000

    update copyright

--- whois.c
+++ whois.c
@@ -1,24 +1,24 @@
-/*******************************************************************************
- A replacement for fwhois/whois.
-
- Copyright (C) 2000,2001 Red Hat, Inc.
- Written by Nalin Dahyabhai <nalin@redhat.com>
- 
- This is free software; you can redistribute it and/or modify it
- under the terms of the GNU General Public License as published by
- the Free Software Foundation; either version 2 of the License, or
- (at your option) any later version.
-
- This program is distributed in the hope that it will be useful, but
- WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with this program; if not, write to the Free Software
- Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-
- ******************************************************************************/
+/*
+ *  Copyright (C) 2015 JoungKyun.Kim <http://oops.org/>
+ *
+ *  This file is part of libipcalc.
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  This program is forked from Nalin Dahyabhai's whois on 2001
+ *    Nalin Dahyabhai <nalin@redhat.com>
+ */
 #ident "$Id$"
 
 #ifdef HAVE_CONFIG_H
